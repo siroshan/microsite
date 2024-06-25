43647de73021638832582311d1f771e7ac2c8ba4
siroshan <siroshang@gmail.com>
2024-06-25 08:39:24 +0530

    chore: Configure project setup

---
+++ src/ui-core/components/molecules/Sections/index.tsx
@@ -0,0 +1,2 @@
+export * from "./CarouselSection";
+export * from "./VideoSection";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,14 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/ui-core/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      // colors: {
+      //   grey: "var(--grey)",
+      // },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +18,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;

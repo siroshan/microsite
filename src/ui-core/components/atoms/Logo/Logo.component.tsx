13719e655fc520bd63f2d6e5cd25b7e97fe1fcdb
siroshan <siroshang@gmail.com>
2024-06-25 19:27:58 +0530

    patch: Change logo props

--- src/ui-core/components/atoms/Logo/Logo.component.tsx
+++ src/ui-core/components/atoms/Logo/Logo.component.tsx
@@ -1,3 +1,15 @@
-export const Logo = () => {
-  return <div className="px-4 py-2 uppercase">Logo</div>;
+import { cn } from "@libs/utils";
+import { LogoProps } from "./Logo.type";
+
+export const Logo = ({ className }: LogoProps) => {
+  return (
+    <div
+      className={cn(
+        "border border-black bg-transparent px-4 py-2 uppercase",
+        className,
+      )}
+    >
+      Logo
+    </div>
+  );
 };
